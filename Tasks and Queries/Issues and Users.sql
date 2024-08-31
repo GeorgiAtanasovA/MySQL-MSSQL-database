85ce2a8b8e14746d741a096ad530496d19cd1693
Georgi <124593129+GeorgiAtanasovA@users.noreply.github.com>
2024-08-31 15:44:34 +0300

    Delete Tasks and Queries directory

--- Tasks and Queries/Issues and Users.sql
+++
@@ -1,8 +0,0 @@
--- Extract from the database, all of the issues, and the users that are assigned to them, so that they end up in the following format:
-
-USE `buhtig`;
-
-SELECT `issues`.`id`, CONCAT(`username`, ': ', `title`) AS 'issue_assignee'
-FROM `issues`
-JOIN `users` ON `users`.`id` = `issues`.`assignee_id`
-ORDER BY `issues`.`id` DESC;
