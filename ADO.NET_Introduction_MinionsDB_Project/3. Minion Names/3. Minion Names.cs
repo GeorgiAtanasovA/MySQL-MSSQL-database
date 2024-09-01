1738ba6d64b5e496f7424073c52f6f43d088c385
Georgi <124593129+GeorgiAtanasovA@users.noreply.github.com>
2024-09-01 14:17:58 +0300

    Update 3. Minion Names.cs

--- ADO.NET_Introduction_MinionsDB_Project/3. Minion Names/3. Minion Names.cs
+++ ADO.NET_Introduction_MinionsDB_Project/3. Minion Names/3. Minion Names.cs
@@ -20,7 +20,6 @@ namespace _3.Minion_Names
          //Write a program that prints on the console all minion names and age for a given villain id, ordered by name alphabetically.
          //If there is no villain with the given ID, print "No villain with ID <VillainId> exists in the database".
          //If the selected villain has no minions, print "(no minions)" on the second row.
-         //------- Решена --------
 
 
          Console.WriteLine(" Who's are the minions of the villain. Choice Villain IDs.");
