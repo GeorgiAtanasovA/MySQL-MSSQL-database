8629123cc990fe86654d98659cf9d9fa793c3d29
Georgi <124593129+GeorgiAtanasovA@users.noreply.github.com>
2024-09-01 14:19:13 +0300

    Update 7. Print All Minion Names.cs

--- ADO.NET_Introduction_MinionsDB_Project/7. Print All Minion Names/7. Print All Minion Names.cs
+++ ADO.NET_Introduction_MinionsDB_Project/7. Print All Minion Names/7. Print All Minion Names.cs
@@ -15,7 +15,7 @@ namespace _7.Print_All_Minion_Names
          //Write a program that prints all minion names from the minions table in the following order:
          //first record, last record, first +1, last - 1, first + 2, last - 2 … first + n, last - n.
          //1 10 2 9 3 8 4 7 5 6
-         //------ Решена -----
+       
 
          //           Example
          //  ----------------- ---------
