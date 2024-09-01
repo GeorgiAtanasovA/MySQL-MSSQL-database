66aec54b92c4de7e962a7798630b1efc1c97d261
Georgi <124593129+GeorgiAtanasovA@users.noreply.github.com>
2024-09-01 13:30:29 +0300

    Delete SQL Tasks directory

--- SQL Tasks/Heavy HTML.sql
+++
@@ -1,9 +0,0 @@
--- There are some pretty big HTML files in the Buhtig database… Unnaturally big. 
--- Extract from the database all of the files, which have size, GREATER than 1000, and their name contains “html”.
-
-USE `buhtig`;
-
-SELECT `id`, `name`, `size`
-FROM `files`
-WHERE `size` > 1000 AND `name` LIKE '%html%'
-ORDER BY `size` DESC;
